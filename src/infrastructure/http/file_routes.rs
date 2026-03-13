c4ed9af143dea93e298870534e13de76e5932ab6
Victor Merino <victor.merino@vidneo.tc>
2026-03-13 14:58:25 +0100

    fixed use_case

--- src/infrastructure/http/file_routes.rs
+++ src/infrastructure/http/file_routes.rs
@@ -7,7 +7,7 @@ use axum::{
 use serde::Deserialize;
 
 use crate::{
-    application::use_cases::GetUploadUrlUseCase,
+    application::use_cases::{GetDownloadUrlUseCase, GetUploadUrlUseCase},
     domain::value_objects::ApiResponse,
     infrastructure::{bootstrap::AppState, http::auth_header_extractor::AuthHeader},
 };
@@ -36,7 +36,7 @@ pub async fn get_download_url(
     _: AuthHeader,
     Path((project_name, file_name)): Path<(String, String)>,
 ) -> impl IntoResponse {
-    let use_case = GetUploadUrlUseCase {
+    let use_case = GetDownloadUrlUseCase {
         file_storage: state.file_storage.clone(),
     };
 
